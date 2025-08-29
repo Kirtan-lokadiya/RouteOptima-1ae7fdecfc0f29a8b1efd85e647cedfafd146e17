2b7e5fe47ca3c92354a81cb491f6fdb8a4f8aa27
Kirtan Lokadiya <kirtanlokadiya998@gmail.com>
2025-08-29 19:12:06 +0530

    Add direct map buttons and use environment variables for secrets

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
+import os
+
 class Config:
-    SECRET_KEY = 'your-secret-key'
-    MONGO_URI = 'mongodb+srv://harsheel:harsheel@auth.kfaj4.mongodb.net/test?retryWrites=true&w=majority&appName=auth'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/routeoptima'
     
     # Flask-Mail configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'kirtanlokadiya998@gmail.com'
-    MAIL_PASSWORD = 'fxxh pqmo upzz osgg'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     
     # Twilio configuration for SMS
-    TWILIO_ACCOUNT_SID='AC35338dd798bacdf069a299b4be6f20b4'
-    TWILIO_AUTH_TOKEN='8b49c53f1dbc496cb024c45c8ee20394'
-    TWILIO_PHONE_NUMBER='+12707139063'
+    TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+    TWILIO_PHONE_NUMBER = os.environ.get('TWILIO_PHONE_NUMBER')
